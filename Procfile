34dbb05f4a284d59600dc26e8510488909c670b3
Lord Cubius <ibrahim1f3shakir@gmail.com>
2021-10-14 03:36:02 +0300

    This is it

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: npm run site-start
-worker: npm run bot-start
+web: npm start
--- bot/index.js
+++ bot/index.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
+exports.start = () => {
+    const { Client, Intents } = require('discord.js');
+    const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+    client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    });
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+    client.on('interactionCreate', async interaction => {
+        
+    });
 
-client.on('interactionCreate', async interaction => {
-
-});
-
-client.login(process.env.DISCORD_TOKEN);
+    client.login(process.env.DISCORD_TOKEN);
+}
